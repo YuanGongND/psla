980b3febfb1f4168b05aac4a5c466f656d8f4612
ygong <yuangongfdu@gmail.com>
2022-05-08 18:09:23 -0400

    fix a bug in dataloader

--- src/dataloaders/audioset_dataset.py
+++ src/dataloaders/audioset_dataset.py
@@ -177,10 +177,14 @@ class AudiosetDataset(Dataset):
         freqm = torchaudio.transforms.FrequencyMasking(self.freqm)
         timem = torchaudio.transforms.TimeMasking(self.timem)
         fbank = torch.transpose(fbank, 0, 1)
+        # this is just to satisfy new torchaudio version.
+        fbank = fbank.unsqueeze(0)
         if self.freqm != 0:
             fbank = freqm(fbank)
         if self.timem != 0:
             fbank = timem(fbank)
+        # squeeze back
+        fbank = fbank.squeeze(0)
         fbank = torch.transpose(fbank, 0, 1)
 
         # normalize the input for both training and test
